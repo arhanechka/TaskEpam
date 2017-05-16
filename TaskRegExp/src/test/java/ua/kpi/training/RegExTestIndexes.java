44a718039b409a2058999e2493ac01db42f94856
arhanechka <arhanechka@gmail.com>
2017-05-16 13:04:02 +0200

    tests were added

---
+++ TaskRegExp/src/test/java/ua/kpi/training/RegExTestIndexes.java
@@ -0,0 +1,72 @@
+package ua.kpi.training;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ua.kpi.training.controller.services.RegExExpressions;
+
+/**
+ * Created by Anya on 16.05.2017.
+ */
+public class RegExTestIndexes {
+    static String[] stringsForTest;
+    static String one;
+    static String two;
+    static String three;
+    static String four;
+    static String five;
+    static String six;
+    static String seven;
+
+    @BeforeClass
+    public static void setArray() {
+        stringsForTest = TestConstants.INDEXES_FOR_TESTING.split(" ");
+        one = stringsForTest[0];
+        two = stringsForTest[1];
+        three = stringsForTest[2];
+        four = stringsForTest[3];
+        five = stringsForTest[4];
+        six = stringsForTest[5];
+        seven = stringsForTest[6];
+    }
+
+    @Test
+    public void nickNameAssertOne() {
+
+        Assert.assertTrue(one, one.matches(RegExExpressions.INDEX_REGEX));
+
+    }
+
+    @Test
+    public void nickNameAssertTwo() {
+        Assert.assertTrue(two, two.matches(RegExExpressions.INDEX_REGEX));
+    }
+
+    @Test
+    public void nickNameAsserThree() {
+        Assert.assertTrue(three, three.matches(RegExExpressions.INDEX_REGEX));
+
+    }
+
+    @Test
+    public void nickNameAssertFour() {
+        Assert.assertTrue(four, four.matches(RegExExpressions.INDEX_REGEX));
+
+    }
+
+    @Test
+    public void nickNameAssertFive() {
+        Assert.assertTrue(five, five.matches(RegExExpressions.INDEX_REGEX));
+    }
+
+    @Test
+    public void nickNameAssertSix() {
+        Assert.assertTrue(six, six.matches(RegExExpressions.INDEX_REGEX));
+    }
+
+    @Test
+    public void nickNameAssertSeven() {
+        Assert.assertTrue(seven, seven.matches(RegExExpressions.INDEX_REGEX));
+
+    }
+}
---
+++ TaskRegExp/src/test/java/ua/kpi/training/TestConstants.java
@@ -0,0 +1,16 @@
+package ua.kpi.training;
+
+/**
+ * Created by Anya on 16.05.2017.
+ */
+public interface TestConstants {
+    String NICK_NAMES_TESTING = "rabbit Rabbit rAbbit r12abbit R45Abbit _rabbit 121";
+    String ALL_NAMES_FOR_TESTING = "Ivanov ivanov iVanov Ivanov1 122 K_iev _Ivan";
+    String PHONE_NUMBERS_FOR_TESTING = "052-234-45-34 050-454-32-91 234-123-34-23 000-234-32-12 00-232-14-23 345-23-23-34 234-234-34-2";
+    String EMAILS_FOR_TESTING = "none@tu.ua none@tutu none_tu.tu nonetutu none.tu@ua none@2er.tr none@tu.2ua";
+    String INDEXES_FOR_TESTING = "12345 00000 123455 1234 10203 1 1t234";
+    String HOUSE_NUMBERS_FOR_TESTING = "1 12 12A 12AS F12 0 01";
+    String FLATS_NUMBERS_FOR_TESTING = "1 12 12A 12AS F12 0 01";
+
+
+}
