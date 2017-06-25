c6c6baff9dd8eda5ee89b0a482c715f7f99466d4
arhanechka <arhanechka@gmail.com>
2017-06-25 16:52:24 +0200

    a few mistakes were corrected

--- Project4Courses/src/com/kpi/arkhipchuk/controller/command/SetStudentMark.java
+++ Project4Courses/src/com/kpi/arkhipchuk/controller/command/SetStudentMark.java
@@ -1,12 +1,14 @@
 package com.kpi.arkhipchuk.controller.command;
 
 import com.kpi.arkhipchuk.model.dao.jdbc.QueryConstants.CourseQueryConstants;
+import com.kpi.arkhipchuk.model.entity.Course;
 import com.kpi.arkhipchuk.model.entity.Student;
 import com.kpi.arkhipchuk.view.AddressConstants;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -16,19 +18,17 @@ import java.util.List;
 public class SetStudentMark extends com.kpi.arkhipchuk.controller.command.Command {
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
         String mark = request.getParameter("mark");
         request.setAttribute("mark", mark);
-        String courseName = request.getParameter("courseName");
-        request.setAttribute("courseName", courseName);
+        Course currentCourse = (Course) session.getAttribute("currentCourse");
 
         int studentId=Integer.parseInt(request.getParameter("studentId"));
         String comment = request.getParameter("comment");
-        int courseId=Integer.parseInt(request.getParameter("courseId"));
-        request.setAttribute("courseId", courseId);
 
         request.setAttribute("comment", comment);
-        service.setStudentMark(mark,comment,studentId,courseId);
-        List<Student> studentListForCurrentCourse = service.findListOfStudentForCourses(CourseQueryConstants.TEACHER_SELECT_LIST_OF_STUDENTS_FOR_CURRENT_COURSES, courseId);
+        service.setStudentMark(mark,comment,studentId,currentCourse.getId());
+        List<Student> studentListForCurrentCourse = service.findListOfStudentForCourses(CourseQueryConstants.TEACHER_SELECT_LIST_OF_STUDENTS_FOR_CURRENT_COURSES, currentCourse.getId());
         request.setAttribute("studentListForCurrentCourse", studentListForCurrentCourse);
         request.getServletContext().getRequestDispatcher(AddressConstants.CURRENT_COURSE_STUDENT_LIST).forward(request, response);
 
